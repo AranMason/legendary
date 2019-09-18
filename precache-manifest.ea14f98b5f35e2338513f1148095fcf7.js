29a9b3680e2a67dc4571e68487ffb69956827da3
Aran Mason <a.mason@hpa.org.nz>
2019-09-18 16:11:16 +1200

    Updates

---
+++ precache-manifest.ea14f98b5f35e2338513f1148095fcf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ff5e437225026ccd3e8899ec1c339f",
+    "url": "/legendary/index.html"
+  },
+  {
+    "revision": "66dbf21092e324eae077",
+    "url": "/legendary/static/css/main.1a24334b.chunk.css"
+  },
+  {
+    "revision": "dee733c7120f54043304",
+    "url": "/legendary/static/js/2.c2601ddf.chunk.js"
+  },
+  {
+    "revision": "66dbf21092e324eae077",
+    "url": "/legendary/static/js/main.22f347c8.chunk.js"
+  },
+  {
+    "revision": "bc0fc617d7ab8f12cd22",
+    "url": "/legendary/static/js/runtime~main.e1e8d4bc.js"
+  },
+  {
+    "revision": "2c0862a67bfb8773d52c7833e17cd5ca",
+    "url": "/legendary/static/media/chevron-down-solid.2c0862a6.svg"
+  },
+  {
+    "revision": "3b30c82204fe750dcaa3c5113585f6ee",
+    "url": "/legendary/static/media/chevron-up-solid.3b30c822.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.43b013eb6a66b23d0abf80b25c8ea34c.js"
+  "/legendary/precache-manifest.ea14f98b5f35e2338513f1148095fcf7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/legendary/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
