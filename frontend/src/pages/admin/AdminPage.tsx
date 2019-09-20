8b2809959dc3231467abc95a499e554b510da99c
Aran Mason <a.mason@hpa.org.nz>
2019-09-20 14:33:40 +1200

    Removed unused import

--- frontend/src/pages/admin/AdminPage.tsx
+++ frontend/src/pages/admin/AdminPage.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Card} from '../../models';
 
-import axios from 'axios';
 import { Link, Redirect } from 'react-router-dom';
 
 import { getUser } from '../../actions/loginActions';
